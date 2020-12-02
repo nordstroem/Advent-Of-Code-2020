1354bacb7d3f4c6781da585ad68c58bbc11e4bad
Johan Nordström <cjs.nordstrom@gmail.com>
2020-12-02 21:39:27 +0100

    Add day 2 solutions

---
+++ days/day2.py
@@ -0,0 +1,28 @@
+import util
+import collections
+import functools
+
+all_lines = util.read_lines("inputs/day2.txt")
+
+def parse(string):
+    (low, high, character, password) = string.replace(":", "").replace("-", " ").split(" ")
+    return (int(low), int(high), character, password)
+
+
+def valid_part1(string):
+    (low, high, character, password) = parse(string)
+    count = list(password).count(character)
+    return count >= low and count <= high
+
+
+def valid_part2(string):
+    (low, high, character, password) = parse(string)
+    return (password[low - 1] == character) != (password[high - 1] == character)
+
+
+def part1():
+    print(util.count_if(all_lines, valid_part1))
+
+
+def part2():
+    print(util.count_if(all_lines, valid_part2))
--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+
 def read_lines(path, fun=lambda x: x):
     with open(path, 'r') as inp:
         lines = inp.readlines()
@@ -7,3 +8,11 @@ def read_lines(path, fun=lambda x: x):
 def read_file(path):
     with open(path, 'r') as inp:
         return inp.read()
+
+
+def count_if(container, predicate):
+    count = 0
+    for element in container:
+        if predicate(element):
+            count = count + 1
+    return count
